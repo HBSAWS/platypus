62dd9669857249941089167e32d2fe365cac047e
Rob Silva <robsilva@gmail.com>
2017-02-28 17:00:09 -0500

    added breadcrumb test and fixed carousel initialization flicker problem

---
+++ test/e2e/breadcrumb.js
@@ -0,0 +1,14 @@
+var config = require('../../nightwatch.conf.BASIC.js');
+
+module.exports = { 
+  'Breadcrumb': function(browser) {
+    browser
+      	.url('http://localhost:3000/Breadcrumb')
+      	.waitForElementVisible('body')
+      	.verify.elementPresent('ol.breadcrumb')
+      	.resizeWindow(300, 800)
+      	.verify.elementPresent('ol.breadcrumb li:nth-child(1).dropdown')
+      	.saveScreenshot('./test/e2e/breadcrumb/breadcrumb.png')
+      	.end();
+  }
+};
