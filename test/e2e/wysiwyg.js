751bd677ffeb51ee409ae54d9c0f001a2efc576a
Rob Silva <robsilva@gmail.com>
2017-03-01 09:53:07 -0500

    added tests for wysiwyg

---
+++ test/e2e/wysiwyg.js
@@ -0,0 +1,12 @@
+var config = require('../../nightwatch.conf.BASIC.js');
+
+module.exports = { 
+  'WYSIWYG': function(browser) {
+    browser
+      	.url('http://localhost:3000/wysiwyg')
+      	.waitForElementVisible('body')
+      	.verify.elementPresent('.note-editor')
+      	.saveScreenshot('./test/e2e/wysiwyg/wysiwyg.png')
+      	.end();
+  }
+};
