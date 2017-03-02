efe5a8057bc2f8b9f0523361fe1c65d725e5c5c5
Rob Silva <robsilva@gmail.com>
2017-03-02 09:24:17 -0500

    renamed db

--- config/env.js
+++ config/env.js
@@ -6,8 +6,7 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 module.exports = {
     development: {
         rootPath: rootPath,
-        // database: 'mongodb://robsilva:1Temporary2@ds111559.mlab.com:11559/platypus',
-        database: 'mongodb://localhost/hbs-aws-framework-dev',
+        database: 'mongodb://localhost/platypus-dev',
         port: process.env.PORT || 3000
     },
     production: {
