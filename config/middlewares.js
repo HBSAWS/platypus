6de5843c4632c7b0b50963f6725b3323e0d0b5d0
Rob Silva <robsilva@gmail.com>
2017-07-20 15:28:16 -0400

    changed res.locals to res.app.locals

--- config/middlewares.js
+++ config/middlewares.js
@@ -20,48 +20,49 @@ module.exports = {
 		return next();
 	},
 	getNav: function(req, res, next) {
+    
         
-       async.waterfall([
-            function(callback) {
+            async.waterfall([
+                function(callback) {
 
-                Category.find({})
-                .populate('_parent')
-                .sort('order')
-                .lean()
-                .exec(function(err, categories) {
-                    if(err) return next(err);
-                    callback(null, categories);
-                });
-
-            },
-            function(categories, callback) {
-                
-                _.map(categories, function(category, i) {
-                    Article.find({
-                        _category: category._id,
-                        version: (res.locals.ver_selected !== res.locals.current ) ? res.locals.ver_selected : res.locals.current
-                    })
-                    .sort('title')
+                    Category.find({})
+                    .populate('_parent')
+                    .sort('order')
                     .lean()
-                    .exec(function(err, a){
-                        if(err) return next(err);           
-                        category.articles = a;
-                        if(categories.length === i+1) {
-                            callback(null, categories);
-                        }
+                    .exec(function(err, categories) {
+                        if(err) return next(err);
+                        callback(null, categories);
                     });
-                });                
-            }
-        ], function (err, categories) {
-            if (err) {
-                return next(err);
-            } else {
-                console.log("Navigation is ready");
-                res.locals.nav = categories;
-                return next();
-            }
-        });
 
+                },
+                function(categories, callback) {
+                    
+                    _.map(categories, function(category, i) {
+                        Article.find({
+                            _category: category._id,
+                            version: (res.locals.ver_selected !== res.locals.current ) ? res.locals.ver_selected : res.locals.current
+                        })
+                        .sort('title')
+                        .lean()
+                        .exec(function(err, a){
+                            if(err) return next(err);           
+                            category.articles = a;
+                            if(categories.length === i+1) {
+                                callback(null, categories);
+                            }
+                        });
+                    });                
+                }
+            ], function (err, categories) {
+                if (err) {
+                    return next(err);
+                } else {
+                    console.log("Navigation is ready");
+                    res.app.locals.nav = categories;
+                    return next();
+                }
+            });
+        
 
 	},
 	debug: function(req, res, next) {
