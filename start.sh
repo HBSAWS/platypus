fd0bef1ed3d89bd35333c58a48daa2ba57435c40
Rob Silva <robsilva@gmail.com>
2018-03-14 09:41:20 -0400

    .

--- start.sh
+++ start.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+export NODE_ENV=production
+export PATH=/export/tools/framework-tools/mongo/bin:/export/tools/framework-tools/node/bin:/export/home/apps/hbs/.local/bin:$PATH
+export PLATYPUS_SESSION_SECRET=1Temporary2
+export PLATYPUS_GOOGLE_MAPS_KEY=AIzaSyDXqmioZvV0UP8vfsXZ-qBmKZ_U-RDnfqA
+export NODE_TLS_REJECT_UNAUTHORIZED="0"
 
-NODE_ENV=production nohup node server.js &
+( nohup node server.js & )
+
+# tail -f nohup.out
